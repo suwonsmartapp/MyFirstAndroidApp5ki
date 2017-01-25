789945c92078b8e87d526734da4f4e0f6a1cc03c
Junsuk Oh <a811219@gmail.com>
2017-01-25 11:20:43 +0900

    커피 앱 연습 : 버튼 클릭, Log, Toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,60 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mMinusButton;
+    private Button mPlusButton;
+    private TextView mQuantityTextView;
+
+    // 수량
+    private int mQuantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // 초기화
+        init();
+
         // 레이아웃 표시
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_coffee);
+
+        // 레이아웃에서 특정 id를 인스턴스 변수와 연결
+        mMinusButton = (Button) findViewById(R.id.minus_button);
+        mPlusButton = (Button) findViewById(R.id.plus_button);
+        mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
+
+        // 무명클래스
+        mMinusButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // debug
+                Log.d(TAG, "마이너스 버튼 클릭");
+                Log.v(TAG, "일반로그");
+                Log.e(TAG, "에러로그");
+                Log.i(TAG, "정보로그");
+                Log.w(TAG, "경고로그");
+
+                // 토스트 메세지
+                Toast.makeText(MainActivity.this,
+                        "메세지",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void init() {
+        mQuantity = 0;
     }
 }
