17819d7d322e284bc59d3c37fc02abf19994653d
Junsuk Oh <a811219@gmail.com>
2017-01-26 09:39:59 +0900

    클릭 이벤트 연습

---
+++ app/src/main/java/com/example/myapplication/BasketballActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class BasketballActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basketball);
+    }
+
+    public void onClickButton(View view) {
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -15,9 +14,6 @@ public class MainActivity extends AppCompatActivity {
     public static final int QUANTITY_MAX = 10;
     public static final int COFFEE_PRICE = 3000;
 
-    private Button mMinusButton;
-    private Button mPlusButton;
-    private Button mOrderButton;
     private TextView mQuantityTextView;
     private TextView mResultTextView;
 
@@ -35,45 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_coffee);
 
         // 레이아웃에서 특정 id를 인스턴스 변수와 연결
-        mMinusButton = (Button) findViewById(R.id.minus_button);
-        mPlusButton = (Button) findViewById(R.id.plus_button);
-        mOrderButton = (Button) findViewById(R.id.order_button);
         mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
         mResultTextView = (TextView) findViewById(R.id.result_text);
 
         // 무명클래스
-        mMinusButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                mQuantity--;
-                if (mQuantity < QUANTITY_MIN) {
-                    mQuantity = QUANTITY_MIN;
-                }
-                displayResult();
-            }
-        });
-
-        // 무명클래스
-        mPlusButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                mQuantity++;
-                if (mQuantity > QUANTITY_MAX) {
-                    mQuantity = QUANTITY_MAX;
-                }
-                displayResult();
-            }
-        });
-
-        mOrderButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = mResultTextView.getText().toString();
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-            }
-        });
+        findViewById(R.id.minus_button).setOnClickListener(this);
+        findViewById(R.id.plus_button).setOnClickListener(this);
+        findViewById(R.id.order_button).setOnClickListener(this);
 
     }
 
@@ -87,5 +51,30 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         mQuantity = 0;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.minus_button:
+                mQuantity--;
+                if (mQuantity < QUANTITY_MIN) {
+                    mQuantity = QUANTITY_MIN;
+                }
+                displayResult();
+                break;
+            case R.id.plus_button:
+                mQuantity++;
+                if (mQuantity > QUANTITY_MAX) {
+                    mQuantity = QUANTITY_MAX;
+                }
+                displayResult();
+                break;
+            case R.id.order_button:
+                String message = mResultTextView.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
