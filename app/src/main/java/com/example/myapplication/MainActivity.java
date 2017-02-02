a9c841e48c882b055147c5fd91363fd79356b501
Junsuk Oh <a811219@gmail.com>
2017-02-02 10:53:33 +0900

    Option Menu 연습

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -111,5 +115,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
+    }
+
+    // 메뉴를 붙이는 부분
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_setting:
+                Toast.makeText(this, "설정 아직 미구현", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_menu2:
+                return true;
+            case R.id.action_menu3:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
