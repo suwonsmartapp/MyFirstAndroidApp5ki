bc55f8f6c886ac95942780d3c46f013203d1cea9
Junsuk Oh <a811219@gmail.com>
2017-03-21 11:32:29 +0900

    PlayerFragment에도 서비스 바인드로 앨범이미지 표시되도록 수정

--- app/src/main/java/com/example/myapplication/fragments/PlayerFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/PlayerFragment.java
@@ -1,13 +1,22 @@
 package com.example.myapplication.fragments;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.myapplication.R;
+import com.example.myapplication.services.MusicService;
 
 /**
  * Created by junsuk on 2017. 3. 9..
@@ -15,9 +24,76 @@ import com.example.myapplication.R;
 
 public class PlayerFragment extends Fragment {
 
+    private MusicService mService;
+    private boolean mBound = false;
+
+    private ImageView mAlbumImageView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.music_player, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mAlbumImageView = (ImageView) view.findViewById(R.id.album_image);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Intent intent = new Intent(getActivity(), MusicService.class);
+        getActivity().bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mBound) {
+            getActivity().unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            MusicService.LocalBinder binder = (MusicService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+
+            // UI 갱신
+            updateUI(mService.isPlaying());
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    private void updateUI(boolean playing) {
+        if (playing) {
+            MediaMetadataRetriever retriever = mService.getMetaDataRetriever();
+            if (retriever != null) {
+                String title = retriever.extractMetadata((MediaMetadataRetriever.METADATA_KEY_TITLE));
+                String artist = retriever.extractMetadata((MediaMetadataRetriever.METADATA_KEY_ARTIST));
+
+                // 오디오 앨범 자켓 이미지
+                byte albumImage[] = retriever.getEmbeddedPicture();
+                if (null != albumImage) {
+                    Glide.with(this).load(albumImage).into(mAlbumImageView);
+                } else {
+                    Glide.with(this).load(R.mipmap.ic_launcher).into(mAlbumImageView);
+                }
+
+            }
+        }
     }
 }
