54c2e2e66790c86fd562abe57db554bbecab19ce
Junsuk Oh <a811219@gmail.com>
2017-03-31 10:58:36 +0900

    Xml 파싱

--- app/src/main/java/com/example/myapplication/activities/NewsXMLActivity.java
+++ app/src/main/java/com/example/myapplication/activities/NewsXMLActivity.java
@@ -17,6 +17,8 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.OkHttpClient;
@@ -67,6 +69,8 @@ public class NewsXMLActivity extends ListActivity {
             return new NewsParser().parse(xml);
         } catch (XmlPullParserException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return null;
     }
@@ -76,6 +80,17 @@ public class NewsXMLActivity extends ListActivity {
         String link;
         String pubDate;
         String category;
+
+        @Override
+        public String toString() {
+            final StringBuffer sb = new StringBuffer("News{");
+            sb.append("title='").append(title).append('\'');
+            sb.append(", link='").append(link).append('\'');
+            sb.append(", pubDate='").append(pubDate).append('\'');
+            sb.append(", category='").append(category).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
     }
 
     private static class NewsAdapter extends BaseAdapter {
@@ -105,9 +120,11 @@ public class NewsXMLActivity extends ListActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ViewHolder holder;
-            if (mmBinding != null) {
+            if (convertView == null) {
                 mmBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
                         R.layout.item_news, parent, false);
+
+                convertView = mmBinding.getRoot();
 
                 holder = new ViewHolder();
 
@@ -132,13 +149,59 @@ public class NewsXMLActivity extends ListActivity {
     }
 
     private static class NewsParser {
-        public List<News> parse(String xml) throws XmlPullParserException {
+        public List<News> parse(String xml) throws XmlPullParserException, IOException {
+
+            List<News> newsList = new ArrayList<>();
+            News news = null;
+            String text = "";
+            boolean isItem = false;
+
             XmlPullParser parser = Xml.newPullParser();
 
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
 
+            parser.setInput(new StringReader(xml));
+            int eventType = parser.getEventType();
+            while(eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = parser.getName();
 
-            return null;
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        if (tagName.equals("item")) {
+                            news = new News();
+                            isItem = true;
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        if (isItem) {
+                            text = parser.getText();
+                        }
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        if (isItem) {
+                            if (tagName.equals("item")) {
+                                newsList.add(news);
+                                isItem = false;
+                            } else if (tagName.equals("title")) {
+                                news.title = text;
+                            } else if (tagName.equals("link")) {
+                                news.link = text;
+                            } else if (tagName.equals("category")) {
+                                news.category = text;
+                            } else if (tagName.equals("pubDate")) {
+                                news.pubDate = text;
+                            }
+                        }
+                        break;
+                    default:
+                }
+
+                eventType = parser.next();
+            }
+
+            return newsList;
         }
     }
 }
