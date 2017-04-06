c4f4a0b67f36741eb5b4042b32d010836829cd18
Junsuk Oh <a811219@gmail.com>
2017-04-06 10:55:45 +0900

    Animation

--- app/src/main/java/com/example/myapplication/activities/AnimationActivity.java
+++ app/src/main/java/com/example/myapplication/activities/AnimationActivity.java
@@ -3,7 +3,13 @@ package com.example.myapplication.activities;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.CycleInterpolator;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.myapplication.R;
@@ -11,17 +17,51 @@ import com.example.myapplication.R;
 public class AnimationActivity extends AppCompatActivity {
 
     ImageView mImageView;
+    EditText mEditText;
+
+    Animation mShakeAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
 
+        mShakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
+        mShakeAnimation.setInterpolator(new CycleInterpolator(7));
+
         mImageView = (ImageView) findViewById(R.id.image_view);
+        mEditText = (EditText) findViewById(R.id.edit_text);
+
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mEditText.startAnimation(mShakeAnimation);
+            }
+        });
     }
 
     public void transition(View view) {
         TransitionDrawable transitionDrawable = (TransitionDrawable) mImageView.getDrawable();
         transitionDrawable.startTransition(500);
+    }
+
+    public void animation1(View view) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.translate);
+        mImageView.startAnimation(animation);
+    }
+
+    public void animation2(View view) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale);
+        mImageView.startAnimation(animation);
     }
 }
